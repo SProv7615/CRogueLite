f926489fa619627b9ef9c9ba1dc354721c4f3ee7
KevinBRitter <ritter_kevin_b@hotmail.com>
2026-07-19 16:07:15 -0600

    creating a simple logger header which outputs to a game log file added header to cmake and used in main updated the roadmap to show implementation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
+#include "helpers/Logger.h"
 #include <SFML/Graphics.hpp>
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 int main()
 {
+    // Configure log verbosity at game startup
+    Logger::getInstance().setLogLevel(LogLevel::DEBUG);
+
+    LOG_INFO("--- Creating new World ---");
+
     sf::RenderWindow window(
         sf::VideoMode({800, 600}),
         "CRogueLite"
@@ -14,12 +20,14 @@ int main()
         {
             if (event->is<sf::Event::Closed>())
             {
+                LOG_INFO("Game Closed");
                 window.close();
             }
         }
         window.clear();
         window.display();
     }
+    LOG_INFO("Exiting main loop");
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
